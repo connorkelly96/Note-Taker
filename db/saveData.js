da30966c730f79510cdb7db9b8f3994ce8bd56c3
connork96 <connorkelly96@yahoo.com>
2022-10-01 15:42:49 -0400

    updated some of the code and added start to package.json

--- db/saveData.js
+++ db/saveData.js
@@ -9,11 +9,11 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 class Save {
     write(note) {
-        return writeNote('db/db.json', JSON.stringify(note));
+        return writeFileAsync('db/db.json', JSON.stringify(note));
     }
 
     read() {
-        return readNote('db/db.json', 'utf8');
+        return readFileAsync('db/db.json', 'utf8');
     }
 
     retrieveNotes() {
